b32f74af836199bbffb2e611ddf261d7a37b8354
alsmirnoff <alex-sm-91@mail.ru>
2024-09-12 18:14:56 +0300

    fix gameOver

--- main.js
+++ main.js
@@ -96,9 +96,10 @@ document.addEventListener('keydown', event => {
 
 function gameOver() {
     cancelAnimationFrame(requestId);
-    this.ctx.fillStyle = 'black';
-    this.ctx.fillRect(1, 3, 8, 1.2);
-    this.ctx.font = '1px Arial';
-    this.ctx.fillStyle = 'red';
-    this.ctx.fillText('GAME OVER', 1.8, 4);
+    
+    ctx.fillStyle = 'black';
+    ctx.fillRect(1, 3, 8, 1.2);
+    ctx.font = '1px Arial';
+    ctx.fillStyle = 'red';
+    ctx.fillText('GAME OVER', 1.8, 4);
 }
